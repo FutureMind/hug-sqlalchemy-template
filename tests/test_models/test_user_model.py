bb926fe608a2676a7ba13fa4ca0b7a7e05bd24d7
t.laszczuk <t.laszczuk@futuremind.com>
2017-03-01 15:55:31 +0100

    Adjust model tests for db session constructor

--- tests/test_models/test_user_model.py
+++ tests/test_models/test_user_model.py
@@ -1,30 +1,33 @@
 from sqlalchemy.sql import exists
 
-from .. import APITest
+from .. import APITest, TESTDB_URI
 from api.models.users import User
-from api.config import db
+from api.db import SQLAlchemy
+from api.app import app
 
 
 class UserModelTestCase(APITest):
 
     def setUp(self):
         super(UserModelTestCase, self).setUp()
-        db.connect()
+        self.db = SQLAlchemy()
+        self.db.init_app(app, TESTDB_URI)
+        self.db.connect()
 
     def tearDown(self):
-        db.close()
+        self.db.close()
         super(UserModelTestCase)
 
     def test_saving_and_retrieving_user(self):
-        count = db.session.query(User).count()
+        count = self.db.session.query(User).count()
         user = User(email='test@email.com', password='TEST')
-        db.session.add(user)
+        self.db.session.add(user)
         self.assertIsNone(user.id)
-        db.session.commit()
-        self.assertEqual(count + 1, db.session.query(User).count())
+        self.db.session.commit()
+        self.assertEqual(count + 1, self.db.session.query(User).count())
         self.assertIsNotNone(user.id)
         self.assertTrue(
-            db.session.query(
+            self.db.session.query(
                 exists().where(User.email == 'test@email.com')
             ).scalar()
         )
